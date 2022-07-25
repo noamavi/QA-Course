5ae5fc4dc1c70a6d56e631ecd52f7dbdad8efb4c
Vlad <vladfeldfix@gmail.com>
2022-07-25 20:08:46 +0300

    v1

--- LSN10/my_store.sql
+++ LSN10/my_store.sql
@@ -1,8 +1,22 @@
 CREATE DATABASE my_store;
 
 CREATE TABLE items(
-	item_ID INT NOT NULL IDENTITY(1,1),
-    item_name VARCHAR(50),
+	item_ID INT NOT NULL UNIQUE IDENTITY(1,1),
+    item_name VARCHAR(20),
     price FLOAT(2),
+    supplier FOREIGN KEY, 
     PRIMARY KEY (item_ID)
 );
+
+USE [my_store]
+GO
+
+INSERT INTO [dbo].[items]
+           ([item_name]
+           ,[price]
+           ,[supplier])
+     VALUES
+    (NiceTable, 1580.20),
+    (Chair, 120.99),
+    (Sofa, 3000.75)
+GO
