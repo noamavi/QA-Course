35c1b113110ec18ff81eacc1972463f3890ff00c
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2022-07-25 22:02:49 +0300

    Update my_store.sql

--- LSN10/my_store.sql
+++ LSN10/my_store.sql
@@ -2,21 +2,14 @@ CREATE DATABASE my_store;
 
 CREATE TABLE items(
 	item_ID INT NOT NULL UNIQUE IDENTITY(1,1),
-    item_name VARCHAR(20),
-    price FLOAT(2),
-    supplier FOREIGN KEY, 
-    PRIMARY KEY (item_ID)
+	item_name VARCHAR(20),
+	price FLOAT(2),
+	supplier INT, 
+	PRIMARY KEY (item_ID)
 );
 
-USE [my_store]
-GO
-
-INSERT INTO [dbo].[items]
-           ([item_name]
-           ,[price]
-           ,[supplier])
-     VALUES
-    (NiceTable, 1580.20),
-    (Chair, 120.99),
-    (Sofa, 3000.75)
-GO
+INSERT INTO [dbo].[items] (item_name, price, supplier)
+VALUES
+	('NiceTable', 1580.20,1),
+	('Chair', 120.99,2),
+	('Sofa', 3000.75,2)
