277b63ed3cfc07adf01a3fe24b43a828fe9df85e
Vlad Feldfix <62237119+VladFeldfix@users.noreply.github.com>
2022-08-23 00:38:01 +0300

    2022-08-23 00:37

--- H2/H2_QUERIES.sql
+++ H2/H2_QUERIES.sql
@@ -1,6 +1,6 @@
-SELECT p_name, price FROM products ORDER BY p_name ASC;
-SELECT * FROM products WHERE price > 50;
-SELECT p_name, price, price*0.2 AS twenty_percent FROM products;
-SELECT * FROM worders WHERE ORDER BY w_name DES;
+SELECT p_name, p_price FROM products ORDER BY p_name ASC;
+SELECT * FROM products WHERE p_price > 50;
+SELECT p_name, p_price, p_price*0.2 AS twenty_percent FROM products;
+SELECT * FROM workers ORDER BY w_lname DESC;
 SELECT TOP 3 * FROM orders WHERE quantity > 10;
 UPDATE orders SET quantity = 10 WHERE w_id = 3;
